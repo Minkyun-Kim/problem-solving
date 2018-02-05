693f8b3032331aa7d0e03b9d621b10a34e83a9d0
mkk0618@naver.com <mkk0618@naver.com>
2018-02-05 15:40:12 +0900

    전위, 중위, 후위 순회의 표본

--- 01991.cpp
+++ 01991.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+vector<vector<char> > v(26, vector<char>(2));
+
+void preorder(char c){
+    cout << c;
+    if(v[c-'A'][0] != 0) 
+        preorder(v[c-'A'][0]);
+    if(v[c-'A'][1] != 0)
+        preorder(v[c-'A'][1]);
+}
+
+void inorder(char c){
+    if(v[c-'A'][0] != 0)
+        inorder(v[c-'A'][0]);
+    cout << c;
+    if(v[c-'A'][1] != 0)
+        inorder(v[c-'A'][1]);
+}
+
+void postorder(char c){
+    if(v[c-'A'][0] != 0)
+        postorder(v[c-'A'][0]);
+    if(v[c-'A'][1] != 0)
+        postorder(v[c-'A'][1]);
+    cout << c;
+}
 
 int main(){
-    vector<vector<char> > v(26, vector<char>(2));
     int n;
+    cin >> n;
     while(n--){
         char c[3];
         cin >> c[0] >> c[1] >> c[2];
@@ -13,10 +38,11 @@ int main(){
         if(c[2] != '.')
             v[c[0]-'A'][1] = c[2];
     }
-    for(int i = 0; i < 26; i++){
-        for(int j = 0; j < 2; j++){
-            cout << v[i][j] << '\n';
-        }
-    }
+    preorder('A');
+    cout << '\n';
+    inorder('A');
+    cout << '\n';
+    postorder('A');
+    cout << '\n';
     return 0;
 }

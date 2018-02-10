3067ee84637422014b1f550fd3bb42f21af7a5db
mkk0618@naver.com <mkk0618@naver.com>
2018-02-10 16:43:07 +0900

    ssh connect

--- practice.cpp
+++ practice.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
-void func(int n1=1, int n2=2, int n3){
-    cout << "func1"<<'\n';
-}
-/*void func(int n1, int n2){
-    cout << "func2"<< '\n';
-}
-*/
+
 int main(){
-    func(1, 2,3);
+    int n;
+    cin >> n;
+
+    int* d = new int[n+1];
+    for(int i = 1; i <= n; i++){
+        d[i] = i;
+        for(int j = 1; j*j <= i; j++){
+            if(d[i] > d[i-j*j] + 1)
+                d[i] = d[i-j*j] + 1;
+        }
+    }
+    cout << d[n];
+    return 0;
 }

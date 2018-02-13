0e9a676e75911cd3919ff8c01a401c15fbf49866
Brandon <mkk0618@naver.com>
2018-02-13 19:06:45 +0900

    쉬운그리디

---
+++ 11399.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            ans += v[j];
+        }
+    }
+    cout << ans << '\n';
+    return 0;
+}

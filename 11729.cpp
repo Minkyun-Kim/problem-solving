b78002a90edea08ee79f2eb045e77dda522b1cb3
Brandon <mkk0618@naver.com>
2018-02-20 19:57:28 +0900

    6-x-y 에 대한 설명 추가. x, y를 제외한 위치값을 구하기 위함

--- 11729.cpp
+++ 11729.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 void solve(int n, int x, int y) {
     if (n == 0) return;
-    solve(n - 1, x, 6 - x - y);
+    solve(n - 1, x, 6 - x - y);//x+y+z=6이고 n을 x에서 y로 옮기려면 n-1개를 x, y가 아닌 곳 즉 6에서 x자리값과 y자리값을 빼야함
     printf("%d %d\n", x, y);
     solve(n - 1, 6 - x - y, y);
 }
--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,31 @@
-aaaaaaaa
+#include <iostream>
+using namespace std;
+
+int in[100000];
+int post[100000];
+int a[100001][2];
+int n;
+void make(int in_start, int in_end, int post_start, int post_end){
+    cout << post[post_end] << ' ';
+    if(in_start >= in_end || post_start >= post_end)
+        return;
+    int i;
+    for(i = in_start; i <= in_end; i++)
+        if(in[i] == post[post_end])
+            break;
+    make(in_start, i-1, post_start, i-1);
+    make(i+1, in_end, post_end - (in_end-i), post_end-1);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> in[i];
+    for(int i = 0; i < n; i++)
+        cin >> post[i];
+    make(0, n-1, 0, n-1);
+    cout << '\n';
+    return 0;
+    
+}

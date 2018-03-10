130153cec2a1d76583161a422eca8c2cd82fd92d
Minkyun Kim <mkk0618@naver.com>
2018-03-10 22:10:45 +0900

    막대기 수는 결국 이진수로 변환했을 때의 1의 갯수

---
+++ 15576.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, cnt = 0;
+    cin >> n;
+    while( n != 0){
+        if(n % 2 == 1)
+            cnt++;
+        n /= 2;
+    }
+    cout << cnt << '\n';
+    return 0;
+}

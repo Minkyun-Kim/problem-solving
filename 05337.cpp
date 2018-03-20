ece7246e46aeeec0b867e5bf30551cc9f6d2ad3a
Minkyun Kim <mkk0618@naver.com>
2018-03-20 22:32:25 +0900

    쉬운 출력

---
+++ 05337.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string k;
+    cout << ".  .   ." << '\n';
+    cout << "|  | _ | _. _ ._ _  _" << '\n';
+    cout << "|/\\|(/.|(_.(_)[ | )(/."<< '\n';
+    return 0;                               
+}
+
+

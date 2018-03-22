0b4f228b83f378b2412b200c0216a63f6eca4656
Minkyun Kim <mkk0618@naver.com>
2018-03-22 16:12:50 +0900

    쉬운 구현. 전체 가격-아는가격 = 모르는가격

---
+++ 05565.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum, price;
+    cin >> sum;
+    for(int i = 0; i < 9; i++){
+        cin >> price;
+        sum -= price;
+    }
+    cout << sum << '\n';
+    return 0;
+}

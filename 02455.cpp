a4817fa39653a5be6e27131934e4670f0a695cae
pushgerm <mkk0618@naver.com>
2018-11-10 20:09:24 +0900

    쉬운 구현문제

---
+++ 02455.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int ans = 0;
+    int out, in, now=0;
+    for(int i = 0; i < 4; i++){
+        cin >> out >> in;
+        now = now - out + in;
+        if(ans < now)
+            ans = now;
+    }
+    cout << ans << '\n';
+    return 0;
+}

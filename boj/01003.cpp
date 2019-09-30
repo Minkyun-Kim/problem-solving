a1e884967fc8d35a9dbb9ccfb18ddb80cae8328d
Minkyun Kim <mkk0618@naver.com>
2019-09-30 23:28:39 +0900

    1003 피보나치 함수
    피보나치 함수의 규칙처럼 n에 대하여 0과 1의 횟수는 n - 1과 n - 2의 0과 1의 횟수와 같다.
    풀이가 안보일땐 규칙을 찾아보도록 하자

--- boj/01003.cpp
+++ boj/01003.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 using namespace std;
 
+int t;
+int n;
+int zero[41] = { 1, 0, 1, 1 };
+int one[41] = { 0, 1, 1, 2 };
 
-static int zero = 0;
-static int one = 0;
-int fibonacci(int n) {
-	if (n == 0) {
-		zero++;
-		return 0;
+int fibo0(int f) {
+	if (f < 4)
+		return zero[f];
+	else if(zero[f] > 0){
+		return zero[f];
 	}
-	else if (n == 1) {
-		one++;
-		return 1;
+	else {
+		return zero[f] = fibo0(f - 1) + fibo0(f - 2);
 	}
-	else
-		return fibonacci(n - 1) + fibonacci(n - 2);
+}
+int fibo1(int f) {
+	if (f < 4)
+		return one[f];
+	else if(one[f] > 0){
+		return one[f];
+	}
+	else {
+		return one[f] = fibo1(f - 1) + fibo1(f - 2);
+	}
 }
 
 int main() {
-	int n;
-	cin >> n;
-	int test;
-	while (n--) {
-		cin >> test;
-		fibonacci(test);
-		cout << zero << " " << one << endl;
-		zero = 0;
-		one = 0;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		if (n == 0) {
+			cout << 1 << ' ' << 0 << '\n';
+		}
+		else if (n == 1) {
+			cout << 0 << ' ' << 1 << '\n';
+		}
+		else {
+			cout << fibo0(n) << ' ' << fibo1(n) << '\n';
+		}
 	}
-	return 0;
 }
